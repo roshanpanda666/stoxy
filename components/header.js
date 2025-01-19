0f9255fa46aff8324d2fbd98153aa116d9302542
roshan <roshanpanda666@gmail.com>
2025-01-19 22:40:31 +0530

    commiting

--- components/header.js
+++ components/header.js
@@ -15,7 +15,7 @@ const Header = () => {
       <a className="mr-5 hover:text-white">First Link</a>
       <a className="mr-5 hover:text-white">Second Link</a>
       <a className="mr-5 hover:text-white">Third Link</a>
-      <a className="mr-5 hover:text-white">Fourth Link</a>
+  
     </nav>
     <button className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">Button
       <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
