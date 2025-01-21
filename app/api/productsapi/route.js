b3ba7e2b4e7882e8234aeffe7e6ff52909c85acd
roshan <roshanpanda666@gmail.com>
2025-01-22 03:55:59 +0530

    commiting

--- app/api/productsapi/route.js
+++ app/api/productsapi/route.js
@@ -1,1 +1,24 @@
 // api routs for crud operation here 
+
+const { connectionSRT } = require("@/app/lib/d");
+const { MongoClient } = require("mongodb");
+const { NextResponse } = require("next/server");
+
+export async function GET(request){
+
+    const uri=connectionSRT
+const client=new MongoClient(uri)
+try{
+    const database = client.db('stock')
+    const inventory = database.collection('inventory')
+
+    const query={}
+    const allproducts= await inventory.find(query).toArry()
+    console.log(allproducts);
+    return NextResponse.json({allproducts})
+
+}
+finally{
+    await client.close()
+}
+}
