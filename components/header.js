5192a865711dc9ec8422e923eb5a099086072b0a
roshan <roshanpanda666@gmail.com>
2025-02-09 20:26:40 +0530

    commiting crime

--- components/header.js
+++ components/header.js
@@ -9,7 +9,7 @@ const Header = () => {
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-blue-500 rounded-full" viewBox="0 0 24 24">
         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
       </svg>
-      <span className="ml-3 text-xl">Stoxy admin </span>
+      <span className="ml-3 text-xl">Stoxy admin page</span>
     </a>
     <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
       <a className="mr-5 hover:text-white">First Link</a>
