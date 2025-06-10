7e6f70470c4baf7f831d7f61747dc63635c0cf81
roshan <roshanpanda666@gmail.com>
2025-06-10 11:15:54 +0530

    Add navigation links using Next.js Link component in Header

--- components/header.js
+++ components/header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Link from 'next/link'
 const Header = () => {
   return (
     <div>
@@ -9,12 +9,17 @@ const Header = () => {
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-blue-500 rounded-full" viewBox="0 0 24 24">
         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
       </svg>
+      <Link href={'/'}>
       <span className="ml-3 text-xl">Stoxy admin page</span>
+      </Link>
     </a>
     <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-      <a className="mr-5 hover:text-white">First Link</a>
-      <a className="mr-5 hover:text-white">Second Link</a>
-      <a className="mr-5 hover:text-white">Third Link</a>
+      <Link href={'/userlist'}>
+      <div className="mr-5 hover:text-white">active users</div>
+
+      </Link>
+      <div className="mr-5 hover:text-white">Second Link</div>
+      <div className="mr-5 hover:text-white">Third Link</div>
   
     </nav>
     <button className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">Button
